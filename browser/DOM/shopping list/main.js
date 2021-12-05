dff319263c7f43ec978b65c886628793a46f086e
HelloMua <jmua333@gmail.com>
2021-12-06 02:20:37 +0900

    실습7. HTML 마크업

---
+++ browser/DOM/shopping list/main.js
@@ -0,0 +1,6 @@
+// const list = document.querySelector('.list');
+// const add = document.querySelector('.add');
+
+// add.addEventListener('click', () => {
+//     const text = document.createElement('text');
+// });
