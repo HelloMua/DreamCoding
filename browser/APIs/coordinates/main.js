4dbbe5a48d1f4073fbf9bceea74b533fb5bec4b5
HelloMua <jmua333@gmail.com>
2021-12-06 00:08:48 +0900

    실습6. 좌표 실습 성능 개선

--- browser/APIs/coordinates/main.js
+++ browser/APIs/coordinates/main.js
@@ -3,17 +3,31 @@ const horizontal = document.querySelector('.horizontal');       // 가로축
 const target = document.querySelector('.target');               // 이미지
 const tag = document.querySelector('.tag');                     // 글자
 
-document.addEventListener('mousemove', (event) => {
-    const x = event.clientX;
-    const y = event.clientY;
-    console.log(`${x}, ${y}`);
+addEventListener('load', () => {
+    const targetRect = target.getBoundingClientRect();
+    console.log(targetRect);
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
 
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
 
-    tag.innerHTML = `${x}px, ${y}px`;
-})
+    document.addEventListener('mousemove', (event) => {
+        const x = event.clientX;
+        const y = event.clientY;
+        console.log(`${x}, ${y}`);
+
+        // left와 top은 layout부터 paint, composition이 발생하기 때문에 비효율적임
+        vertical.style.transform = `translateX(${x}px)`;
+        horizontal.style.transform = `translateY(${y}px)`;
+        target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+        tag.style.transform = `translate(${x}px, ${y}px)`;
+
+        // vertical.style.left = `${x}px`;
+        // horizontal.style.top = `${y}px`;
+        // target.style.left = `${x}px`;
+        // target.style.top = `${y}px`;
+        // tag.style.left = `${x}px`;
+        // tag.style.top = `${y}px`;
+
+        tag.innerHTML = `${x}px, ${y}px`;
+    })
+});
