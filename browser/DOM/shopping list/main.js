c26fff60a9f435330684835edf405651d5a4aa5e
HelloMua <jmua333@gmail.com>
2021-12-07 01:32:56 +0900

    실습7. CSS 스타일링

--- browser/DOM/shopping list/main.js
+++
@@ -1,6 +0,0 @@
-// const list = document.querySelector('.list');
-// const add = document.querySelector('.add');
-
-// add.addEventListener('click', () => {
-//     const text = document.createElement('text');
-// });
