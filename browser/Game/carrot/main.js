713c84e7097ecc7c90a20e04280b4111b3d5d8d8
HelloMua <jmua333@gmail.com>
2021-12-11 21:53:21 +0900

    게임 시작하고, 타이머 설정하기

--- browser/Game/carrot/main.js
+++ browser/Game/carrot/main.js
@@ -1,15 +1,77 @@
 'use strict';
 
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();        // field의 전체적인 사이즈와 포지션을 알 수 있음
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', () => {
+    if (started) {
+        stopGame();
+    } else {
+        startGame();
+    }
+
+    started = !started;
+});
+
+function stopGame() {
+    
+}
+
+function startGame() {
+    initGame();
+    showStopBtn();
+    showTimerAndScore();
+    startGameTimer();
+}
+
+function showStopBtn() {
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+
+function startGameTimer() {
+    let remainingTimeSec = GAME_DURATION_SEC;
+    updateTimerText(remainingTimeSec);
+    timer = setInterval(() => {
+        if (remainingTimeSec <= 0) {
+            clearInterval(timer);
+            return;
+        }
+        updateTimerText(--remainingTimeSec);
+    }, 1000);
+}
+
+function updateTimerText(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    gameTimer.innerHTML = `${minutes}:${seconds}`;
+}
 
 function initGame() {
+    field.innerHTML = '';
+    gameScore.innerHTML = CARROT_COUNT;
     // 벌레와 당근을 생성한 뒤 field에 추가해줌
-    console.log(fieldRect);
-    addItem('carrot', 5, 'img/carrot.png');
-    addItem('bug', 5, 'img/bug.png');
+    // console.log(fieldRect);
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+    addItem('bug', CARROT_COUNT, 'img/bug.png');
 }
 
 function addItem(className, count, imgPath) {
@@ -36,4 +98,3 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-initGame();
